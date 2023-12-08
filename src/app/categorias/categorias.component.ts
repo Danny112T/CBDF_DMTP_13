9f9bfd491e2f215e5b0e1a0943c08315226d1baa
Daniel Tovar <91617620+Danny112T@users.noreply.github.com>
2023-12-08 15:16:35 -0600

    Fix image source and add functionality to add, edit, and delete categories

--- src/app/categorias/categorias.component.ts
+++ src/app/categorias/categorias.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-categorias',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './categorias.component.html',
   styleUrl: './categorias.component.css'
 })
 export class CategoriasComponent {
+  categoria = {
+    id: 0,
+    nombre: "",
+    descripcion: "",
+  };
 
+  categorias = [
+    {id: 1, nombre: "Zapateria", descripcion: "Todo lo relacionado con zapatos"},
+    {id: 2, nombre: "Ropa", descripcion: "Todo lo relacionado con ropa"},
+    {id: 3, nombre: "Hogar", descripcion: "Todo lo relacionado con el hogar"},
+    {id: 4, nombre: "Tecnologia", descripcion: "Todo lo relacionado con tecnologia"},
+    {id: 5, nombre: "Deportes", descripcion: "Todo lo relacionado con deportes"},
+  ];
+
+  agregarCategoria() {
+    const posId = this.categorias.findIndex((cat) => cat.id == this.categoria.id);
+    if (this.categoria.id>0 || posId!=-1) {
+      alert("vas a agregar la categoria con nombre " + this.categoria.nombre);
+      this.categorias.push(this.categoria);
+    } else {
+      alert("Error: verifica tus datos")
+      console.log(this.categoria.nombre)
+    }
+  }
+  seleccionarCategoria() {}
+  eliminarCategoria(id: number) {
+    alert("Eliminar categoria con id: " + id + " :p")
+    const posId = this.categorias.findIndex((categoria) => categoria.id == id);
+    this.categorias.splice(posId, 1);
+  }
+  actualizarCategoria() {}
 }
